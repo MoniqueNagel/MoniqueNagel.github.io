d62b70973bd9e5cf9c6706bc0f8f9bb7335c49d8
MoniqueNagel <moniquenagel24.mn@gmail.com>
2020-10-03 11:42:36 +0200

    Ofline changes that were made: Modification of UI, Usage of jquery, Creation of eventlisteners for web elements (scroll, navabar, button)

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+/*==========================
+   PRELOADER
+============================*/
+(function($){
+	"use strict";
+
+	//Show preloader until webpage is fully loaded
+	$(window).ready(function(){
+		window.onload=function(){
+			$('#loader').fadeOut(500,function(){
+				$('#loader').remove();
+			});
+		}
+	});
+})(jQuery);
+
+
+/*==========================
+   EVENT LISTENERS
+============================*/
+/*navbar*/
+/*window.onscroll=function(){stickyNavbar()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function stickyNavbar(){
+	if(window.pageYOffset > sticky){
+		navbar.classList.add("sticky");
+	}
+	else{
+		navbar.classList.remove("sticky");
+	}
+}*/
+
+
